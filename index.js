9b71fd8ef90e39bd71e8791da04fb2d76c936a1d
lukc30 <carvalholucas30@gmail.com>
2024-06-11 20:31:19 -0300

    Backend ferito em node

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routerLivro = require('./route/routesLivro');
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors())
+app.use('/', routerLivro);
+
+app.listen(5000, ()=>{
+    console.log('servidor rodandoooooooooooo - http://localhost:5000');
+});
